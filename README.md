3c6d9b06eeb03ea7141ee93503f80690d203c62d
Konstantin Honcharov <k.honcharov@gmail.com>
2026-05-26 17:15:55 +0300

    change polling interval

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Sensor HUB
+
+Config:
+
+- Arduino Nano - Old bootloader
+- SPS = 250
+- channels: 2 out of 4 available
+- sens polling interval: 50 ms
--- src/prod/index/index.ino
+++ src/prod/index/index.ino
@@ -3,7 +3,7 @@
 #define PORT_SPEED 115200
 
 #define INIT_DELAY 5000
-#define POLLING_INTERVAL 100
+#define POLLING_INTERVAL 50
 
 #define MODULE_1_ADDRESS 0x48
 #define CHANNEL_1 0
@@ -43,35 +43,39 @@ void loop() {
 
     sensValue1 = converterModule.readADC_SingleEnded(CHANNEL_1);
     sensValue2 = converterModule.readADC_SingleEnded(CHANNEL_2);
-    sensValue3 = converterModule.readADC_SingleEnded(CHANNEL_3);
-    sensValue4 = converterModule.readADC_SingleEnded(CHANNEL_4);
+    sensValue3 = 0;
+    sensValue4 = 0;
 
+    // Init bytes
     frame[0] = 0;
     frame[1] = 0;
     frame[2] = 0;
+
     frame[3] = MODULE_1_ADDRESS;
+
+    // Control bytes
+    frame[4] = 1;
+    frame[7] = 2;
+    frame[10] = 3;
+    frame[13] = 4;
+    frame[16] = 5;
+    frame[19] = 6;
 
     frame[9] = lowByte(currentTimeStamp);
     currentTimeStamp >>= 8;
     frame[8] = lowByte(currentTimeStamp);
     currentTimeStamp >>= 8;
-    frame[7] = 2;
     frame[6] = lowByte(currentTimeStamp);
     currentTimeStamp >>= 8;
     frame[5] = lowByte(currentTimeStamp);
     currentTimeStamp >>= 8;
-    frame[4] = 1;
 
-    frame[10] = 3;
     frame[11] = highByte(sensValue1);
     frame[12] = lowByte(sensValue1);
-    frame[13] = 4;
     frame[14] = highByte(sensValue2);
     frame[15] = lowByte(sensValue2);
-    frame[16] = 5;
     frame[17] = highByte(sensValue3);
     frame[18] = lowByte(sensValue3);
-    frame[19] = 6;
     frame[20] = highByte(sensValue4);
     frame[21] = lowByte(sensValue4);
 
