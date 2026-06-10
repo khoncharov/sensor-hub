99164a9e8c2e609c9c8973fa8d30643127d4808f
Konstantin Honcharov <k.honcharov@gmail.com>
2026-06-10 15:15:50 +0300

    feat: add sens data averaging

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,9 +1,11 @@
 #include <Adafruit_ADS1X15.h>
 
 #define PORT_SPEED 115200
-
 #define INIT_DELAY 5000
-#define POLLING_INTERVAL 50
+
+// number of points for averaging
+#define ITEMS_NUM_TO_SEND 4
+#define POLL_INTERVAL 25
 
 #define MODULE_1_ADDRESS 0x48
 #define CHANNEL_1 0
@@ -13,23 +15,30 @@
 
 Adafruit_ADS1115 converterModule;
 
-const byte FRAME_LEN = 22;  // byte
-byte frame[FRAME_LEN];
+const uint8_t FRAME_LEN = 22;
+uint8_t frame[FRAME_LEN];
 
-uint16_t sensValue1;
-uint16_t sensValue2;
-uint16_t sensValue3;
-uint16_t sensValue4;
+uint32_t channel1ValueSum = 0;
+uint32_t channel2ValueSum = 0;
+uint32_t channel3ValueSum = 0;
+
+uint16_t channel1AvgValue = 0;
+uint16_t channel2AvgValue = 0;
+uint16_t channel3AvgValue = 0;
+uint16_t channel4AvgValue = 0;
 
 uint32_t lastSensPollTime;
 uint32_t currentTime;
+
+uint8_t itemsNum = 0;
 
 void setup() {
   lastSensPollTime = millis();
 
   Serial.begin(PORT_SPEED);
 
-  converterModule.setDataRate(RATE_ADS1115_250SPS);  // 250 samples per second - 860 max, see <Adafruit_ADS1X15.h>
+  // transfer rates see <Adafruit_ADS1X15.h>
+  converterModule.setDataRate(RATE_ADS1115_250SPS);
   converterModule.begin();
 
   delay(INIT_DELAY);
@@ -38,13 +47,29 @@ void setup() {
 void loop() {
   currentTime = millis();
 
-  if (currentTime - lastSensPollTime >= POLLING_INTERVAL) {
+  if (currentTime - lastSensPollTime >= POLL_INTERVAL) {
     lastSensPollTime = currentTime;
 
-    sensValue1 = converterModule.readADC_SingleEnded(CHANNEL_1);
-    sensValue2 = converterModule.readADC_SingleEnded(CHANNEL_2);
-    sensValue3 = 0;
-    sensValue4 = 0;
+    channel1ValueSum += converterModule.readADC_SingleEnded(CHANNEL_1);
+    channel2ValueSum += converterModule.readADC_SingleEnded(CHANNEL_2);
+    channel3ValueSum += converterModule.readADC_SingleEnded(CHANNEL_3);
+
+    itemsNum += 1;
+
+    if (itemsNum < ITEMS_NUM_TO_SEND) {
+      continue;
+    }
+
+    channel1AvgValue = channel1ValueSum / ITEMS_NUM_TO_SEND;
+    channel2AvgValue = channel2ValueSum / ITEMS_NUM_TO_SEND;
+    channel3AvgValue = channel3ValueSum / ITEMS_NUM_TO_SEND;
+    channel4AvgValue = 0xffff; // channel 4 not used, but retained for future implementations
+
+    channel1ValueSum = 0;
+    channel2ValueSum = 0;
+    channel3ValueSum = 0;
+    
+    itemsNum = 0;
 
     // Init bytes
     frame[0] = 0;
@@ -66,14 +91,14 @@ void loop() {
     frame[6] = (lastSensPollTime >> 16) & 0xff;
     frame[5] = (lastSensPollTime >> 24) & 0xff;
 
-    frame[11] = (sensValue1 >> 8) & 0xff;
-    frame[12] = sensValue1 & 0xff;
-    frame[14] = (sensValue2 >> 8) & 0xff;
-    frame[15] = sensValue2 & 0xff;
-    frame[17] = (sensValue3 >> 8) & 0xff;
-    frame[18] = sensValue3 & 0xff;
-    frame[20] = (sensValue4 >> 8) & 0xff;
-    frame[21] = sensValue4 & 0xff;
+    frame[11] = (channel1AvgValue >> 8) & 0xff;
+    frame[12] = channel1AvgValue & 0xff;
+    frame[14] = (channel2AvgValue >> 8) & 0xff;
+    frame[15] = channel2AvgValue & 0xff;
+    frame[17] = (channel3AvgValue >> 8) & 0xff;
+    frame[18] = channel3AvgValue & 0xff;
+    frame[20] = (channel4AvgValue >> 8) & 0xff;
+    frame[21] = channel4AvgValue & 0xff;
 
     Serial.write(frame, FRAME_LEN);
   }
